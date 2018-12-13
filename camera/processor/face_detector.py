28a9765024b16d5460e2938fcd1b547e3303f7c8
sekin3 <45744141+sekin3@users.noreply.github.com>
2018-12-13 20:01:54 +0900

    Update face_detector.py

--- camera/processor/face_detector.py
+++ camera/processor/face_detector.py
@@ -42,7 +42,27 @@ class FaceDetector(object):
         for (x,y,w,h) in faces:
             cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,0),2)
             num++
+        #スケールの書き込み関数
+        def make_scale(im,length=40,from_edge = 15,thick = 2,hight = 6, font_size = 0.6,pix_size = 10):
 
-        putText(num)
+        w = im.shape[0]
+        h = im.shape[1]
+            #横線
+        cv2.line(im,(w-length-from_edge,h-from_edge),(w-from_edge,h-from_edge),(255,255,0),thick)
+        #縦線左
+        cv2.line(im,(w-length-from_edge,h-from_edge-hight/2),(w-length-from_edge,h-from_edge+hight/2),(255,255,0),thick)
+        #縦線右
+        cv2.line(im,(w-from_edge,h-from_edge-hight/2),(w-from_edge,h-from_edge+hight/2),(255,255,0),thick)
+
+        #1ピクセルのサイズから長さを計算
+        size = pix_size*length
+        text = str(num)
+        #フォントの指定
+        #font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+        #font = cv2.FONT_HERSHEY_SIMPLEX
+        font = cv2.FONT_HERSHEY_PLAIN
+        #文字の書き込み
+        cv2.putText(im,text,(w-length-from_edge-5,h-from_edge-hight),font, font_size,(255,255,0))
+
         # frameを戻り値として返す
         return frame
